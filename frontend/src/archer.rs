49206f73b2c15b737dd9b5d9f0802d27249c6e1f
ThoFrank <thomas@franks-im-web.de>
2024-11-07 20:45:14 +0100

    cargo clippy --fix

--- frontend/src/archer.rs
+++ frontend/src/archer.rs
@@ -59,8 +59,7 @@ impl ArcherModel {
             .contains(&self.selected_target_face)
         {
             self.selected_target_face = *self
-                .possible_target_faces
-                .get(0)
+                .possible_target_faces.first()
                 .unwrap_or(&TargetFace::M18Spot);
         }
     }
@@ -73,7 +72,7 @@ impl ArcherModel {
             DoB::Invalid(_) => Vec::new(),
         };
 
-        let new_cls = match (self.cls, available_classes.get(0)) {
+        let new_cls = match (self.cls, available_classes.first()) {
             (Some(cls), Some(&new)) => {
                 if available_classes.contains(&cls) {
                     return;
