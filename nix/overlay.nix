93e5ae0d22ccad773b539e0b97c5b40a618d9dec
Thomas Frank <thomas@franks-im-web.de>
2024-11-30 23:39:46 +0100

    fix nix build

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -1,15 +1,15 @@
 (final: prev: {
   wasm-bindgen-cli = prev.wasm-bindgen-cli.overrideAttrs (old: {
-    version = "0.2.93";
+    version = "0.2.97";
     src = prev.fetchCrate {
       pname = "wasm-bindgen-cli";
       version = final.wasm-bindgen-cli.version;
-      sha256 = "sha256-DDdu5mM3gneraM85pAepBXWn3TMofarVR4NbjMdz3r0=";
+      sha256 = "sha256-DDUdJtjCrGxZV84QcytdxrmS5qvXD8Gcdq4OApj5ktI=";
     };
     cargoDeps = old.cargoDeps.overrideAttrs (_: {
       src =  final.wasm-bindgen-cli.src; # You need to pass "src" here again,
                                          # otherwise the old "src" will be used.
-      outputHash = "sha256-birrg+XABBHHKJxfTKAMSlmTVYLmnmqMDfRnmG6g/YQ=";
+      outputHash = "sha256-l3Wi2h3773u9HwS2+D1SWiddolh9qK1eI4uFfrwvxsY=";
     });
   });
   trunk = prev.trunk.overrideAttrs (old: {
