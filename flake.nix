230010ef08ddb8fa6d1702ddc32cc4e4e8104f65
Thomas Frank <thomas@franks-im-web.de>
2025-03-28 20:25:00 +0100

    Set enable info log by default in container

--- flake.nix
+++ flake.nix
@@ -52,6 +52,7 @@
               paths = [ pkgs.cacert ];
             };
             config.Cmd = ["${backend}/bin/backend"];
+            config.Env = ["RUST_LOG=info"];
           };
         };
 
